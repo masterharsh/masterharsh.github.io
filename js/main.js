0666b6443991a807c9060334f5a8487f413c7c68
Harsh Jain <masterharshjain@gmail.com>
2020-08-31 13:32:01 +0530

    error checks

--- js/main.js
+++ js/main.js
@@ -53,43 +53,6 @@ window.onload = () => {
       $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);    
     }  
     
-    function generate_button_message(msg, buttons){    
-      /* Buttons should be object array 
-        [
-          {
-            name: 'Existing User',
-            value: 'existing'
-          },
-          {
-            name: 'New User',
-            value: 'new'
-          }
-        ]
-      */
-      INDEX++;
-      var btn_obj = buttons.map(function(button) {
-         return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
-      }).join('');
-      var str="";
-      str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
-      str += "          <span class=\"msg-avatar\">";
-      str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
-      str += "          <\/span>";
-      str += "          <div class=\"cm-msg-text\">";
-      str += msg;
-      str += "          <\/div>";
-      str += "          <div class=\"cm-msg-button\">";
-      str += "            <ul>";   
-      str += btn_obj;
-      str += "            <\/ul>";
-      str += "          <\/div>";
-      str += "        <\/div>";
-      $(".chat-logs").append(str);
-      $("#cm-msg-"+INDEX).hide().fadeIn(300);   
-      $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
-      $("#chat-input").attr("disabled", true);
-    }
-    
     $(document).delegate(".chat-btn", "click", function() {
       var value = $(this).attr("chat-value");
       var name = $(this).html();
@@ -110,6 +73,7 @@ window.onload = () => {
     $(".clear-chat").click(function(e){
       var chatLogs = document.getElementById('chatLogs');
       chatLogs.innerHTML = "";
+      $('#chat-input').val('');
     });
 /////////////////SPEECH RECOGNITION/////////////////////////////////////////////
 const log = document.querySelector('.output_log');
@@ -221,43 +185,50 @@ function fetchCommandResponse(textInput, data){
   }
 
   if (textInput.indexOf("stop")!== -1){
-    console.log("stop");
     synth.cancel();
     $('#chat-input').val('');
+  } else {
+      $.ajax("http://pprpatha-1.subnet1phx1.devcecphx.oraclevcn.com:5002/api/workflow", {
+        type: "POST",
+        async: false,
+        contentType: "application/json",
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+        },
+        data: JSON.stringify( messageObject ),
+        statusCode: {
+          500: function(xhr) {
+            console.log("500: Internal Server Error");
+            var defaultMessage = "I am sorry, I could not find it."
+            generate_message(defaultMessage, 'user'); 
+            synthVoice(defaultMessage,recogLang);
+            $('#chat-input').val('');
+          }
+        },
+        success: function(data, textStatus,xhr) {
+        previousMessage = data;
+        if(data.result && data.result.message !== ""){
+            window.setTimeout( function(){
+              generate_message(data.result.message, 'user'); 
+              synthVoice(data.result.message,recogLang); 
+              $('#chat-input').val('');
+            }, 1000);
+          } else {
+            var defaultMessage = "I am sorry, I think I did not understand."
+            generate_message(defaultMessage, 'user'); 
+            synthVoice(defaultMessage,recogLang);
+            $('#chat-input').val('');
+          }
+          
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(errorThrown);
+        }
+      });
   }
 
-  $.ajax("http://pprpatha-1.subnet1phx1.devcecphx.oraclevcn.com:5002/api/workflow", {
-          type: "POST",
-          async: false,
-          contentType: "application/json",
-          beforeSend: function(xhr) {
-              xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-          },
-          data: JSON.stringify( messageObject ),
-          success: function(data) {
-            console.log(data);
-           //synthVoice(data.result.message, 'en-US');
-           previousMessage = data;
-            if( data.result && data.result.message !== ""){
-              window.setTimeout( function(){
-                generate_message(data.result.message, 'user'); 
-                synthVoice(data.result.message,recogLang); 
-                $('#chat-input').val('');
-              }, 1000);
-             
-            } else {
-              var defaultMessage = "I am sorry, I think I did not understand."
-              generate_message(defaultMessage, 'user'); 
-              synthVoice(defaultMessage,recogLang);
-              $('#chat-input').val('');
-            }
-            
-    
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown);
-          }
-        });
+ 
     }        
 
 }
